8d3453b8562e4049b08187e609572579417e8303
Aki Kakehi <197594452+Aki-rabbit@users.noreply.github.com>
2025-09-19 13:49:52 +0900

    factorybotの活用

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Micropost, type: :model do
-  let(:user) { User.create!(name: "Bob", email: "bob@example.com") }
+  let(:user){create(:user)}
 
-  describe "バリデーション" do
-    it "content が140文字以内なら有効" do
-      micropost = Micropost.new(content: "a" * 140, user: user)
+  describe 'バリデーション' do
+    it 'content が140文字以内なら有効' do
+      micropost = build(:micropost, content: 'a' * 140, user: user)
       expect(micropost).to be_valid
     end
 
-    it "content が141文字以上だと無効" do
-      micropost = Micropost.new(content: "a" * 141, user: user)
+    it 'content が141文字以上だと無効' do
+      micropost = build(:micropost, content: 'a' * 141, user: user)
       expect(micropost).not_to be_valid
     end
 
-    it "user が紐づいていないと無効" do
-      micropost = Micropost.new(content: "Hello")
+    it 'user が紐づいていないと無効' do
+      micropost = build(:micropost, content: 'Valid content', user: nil)
       expect(micropost).not_to be_valid
     end
   end
 
-  describe "関連付け" do
+  describe '関連付け' do
     it { is_expected.to belong_to(:user) }
   end
 end
